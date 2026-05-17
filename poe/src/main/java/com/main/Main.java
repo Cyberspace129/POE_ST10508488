deffd03086d03aa5ded1cf144b8183d19baa68f7
Cyberspace129 <129gbc@gmail.com>
2026-05-17 19:56:59 +0200

    feat: Implement main application loop with user interaction menus

--- poe/src/main/java/com/main/Main.java
+++ poe/src/main/java/com/main/Main.java
@@ -2,28 +2,91 @@ package com.main;
 
 import java.util.Scanner;
 
-import com.main.StoredUser;
-import com.main.RegLog;
+import com.main.StoredUser.*;
+import com.main.RegLog.*;
 
 public class Main {
+    public static void PrintOptionMenu() {
+        System.out.println("1) Register a New user.");
+        System.out.println("2) Login to existing user.");
+        System.out.println("3) Exit app.");
+    }
+
+    public static void PrintMainMenu() {
+        System.out.println("1) Send a new message.");
+        System.out.println("2) View previous messages.");
+        System.out.println("3) Exit app.");
+    }
+
+    public static StoredUser.User PrintRegistorMenu() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("What is your full name?\n:");
+        String FullName = in.nextLine();
+
+        System.out.println("What is your username? \n:");
+        String Username = in.nextLine();
+
+        System.out.println("Enter your South African Phone number? \n:");
+        String Phonenumber = in.nextLine();
+
+        System.out.println("Enter your Email? \n:");
+        String Email = in.nextLine();
+
+        System.out.println("Enter your Password \n:");
+        String Password = in.nextLine();
+
+        return new User(new RegLog.Registration(FullName, Phonenumber, Email, Username, Password));
+    }
+
+    public static boolean CanLogin(StoredUser.User User) {
+        Scanner in = new Scanner(System.in);
+        boolean IsCorrectUserName = true;
+        boolean IsCorrectUserPass = true;
+
+        if (IsCorrectUserName & IsCorrectUserPass) {
+            System.out.println("welcom back " + User.UserName + ".");
+        }
+        System.out.println("Enter Username\n:");
+        String username = in.nextLine();
+
+        System.out.println("Enter Password\n:");
+        String password = in.nextLine();
+
+        if (!username.equals(User.UserName)) {
+            IsCorrectUserName = false;
+        }
+        if (!password.equals(User.Password))
+
+            if (!(IsCorrectUserName & IsCorrectUserPass)) {
+                System.out.println("Ether Password or Username does not match.");
+            }
+
+        return IsCorrectUserName & IsCorrectUserPass;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        StoredUser.User User;
-
         boolean isRunning = true;
+        StoredUser.User TestUser = new User(
+                new RegLog.Registration("Human Person", "+27111111111", "example@gmail.com", "Bot_", "I'mABot2"));
 
         while (isRunning) {
-            System.out.println("What is your username? \n:");
-            String Username = in.nextLine();
-            System.out.println("Enter your South African Phone number? \n:");
-            String Phonenumber = in.nextLine();
-            System.out.println("");
-            String Email = Email;
-            String Password = Password;
+            PrintOptionMenu();
 
-            // has no data
-            User = StoredUser.User();
+            boolean LoggedIn = false;
+
+            String UserOption = in.next();
+
+            StoredUser.User MainUser;
+
+            if (UserOption.equals("1")) {
+                MainUser = PrintRegistorMenu();
+            } else if (UserOption.equals("2")) {
+                LoggedIn = CanLogin(MainUser);
+            }
+
         }
 
         in.close();
